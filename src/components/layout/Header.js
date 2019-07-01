7736a2ba60a27b34334fdd3290427db1d0e82f0a
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-07-01 16:47:08 -0700

    BG Header img, gradient overlay

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -6,12 +6,15 @@ const Header = () => {
     <div
       className={"dashboard_header"}
       style={{
-        background: `linear-gradient(180deg, rgba(0, 0, 0, 0.8), rgba(34, 34, 31, 1)), url('${headerImage}')`,
+        background: `linear-gradient(180deg, rgba(0, 0, 0, 0.6), rgba(34, 34, 31, 1)), url('${headerImage}')`,
+        backgroundPositionY: "center",
       }}>
       {/* <img className={ "dashboard_header-img" } src={ headerImage }/> */}
       <h1 className={"dashboard_header-user-name"}>Name Placeholder</h1>
       <h3 className={"dashboard_header-city"}>City Name</h3>
-      <i className="fas fa-trophy" />
+      <h3 className={"dashboard_header-city"}>
+        <i className="fas fa-trophy" />0
+      </h3>
     </div>
   )
 }
