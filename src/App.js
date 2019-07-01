8b018b30666a8ed14f4fe345f20aa3fcf10cc82d
jeremiahtenbrink <jeremiahtenbrink@gmail.com>
2019-07-01 09:46:04 -0600

    forgot to add the custom auth hook to app.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { Route } from "react-router";
+import { withRouter } from "react-router";
 
 function App(){
+    const state = useAuth( props.history );
     return ( <div>
-            <h1>Firebase Template</h1>
-            <Route exact path='/' render={ props => <h1>root</h1> }/>
-            <Route path='/login' render={ props => <Login { ...props } /> }/>
-            <Route path='/signup' render={ props => <Login { ...props } /> }/>
-            <Route exact path='/home' render={ props => <h1>home</h1> }/>
-            <Route path={ "/dashboard" }
-                   render={ props => <Dashboard { ...props } /> }/>
-        </div> );
+        <h1>Firebase Template</h1>
+        <Route exact path='/' render={ props => <h1>root</h1> }/>
+        <Route path='/login' render={ props => <Login { ...props } /> }/>
+        <Route path='/signup' render={ props => <Login { ...props } /> }/>
+        <Route exact path='/home' render={ props => <h1>home</h1> }/>
+        <Route path={ "/dashboard" }
+               render={ props => <Dashboard { ...props } /> }/>
+    </div> );
 }
 
-export default App;
+export default withRouter( App );
