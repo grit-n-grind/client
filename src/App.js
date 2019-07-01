360b5a114aaa6b0bca270d3135d3a00c16529cf0
jeremiahtenbrink <jeremiahtenbrink@gmail.com>
2019-07-01 09:41:28 -0600

    added some hooks from other apps.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
+import { Route } from "react-router";
 
-function App() {
-  return (
-    <div>
-      <h1>Firebase Template</h1>
-    </div>
-  );
+function App(){
+    return ( <div>
+            <h1>Firebase Template</h1>
+            <Route exact path='/' render={ props => <h1>root</h1> }/>
+            <Route path='/login' render={ props => <Login { ...props } /> }/>
+            <Route path='/signup' render={ props => <Login { ...props } /> }/>
+            <Route exact path='/home' render={ props => <h1>home</h1> }/>
+            <Route path={ "/dashboard" }
+                   render={ props => <Dashboard { ...props } /> }/>
+        </div> );
 }
 
 export default App;
---
+++ src/hooks/useAuth.js
@@ -0,0 +1,38 @@
+import React, { useContext, useState } from "react";
+import firebase from "../config/firebase";
+
+export const userContext = React.createContext( {
+    user: null, initializing: false,
+} );
+
+export const useSession = () => {
+    const { user } = useContext( userContext );
+    return user;
+};
+
+export const useAuth = ( history ) => {
+    const [ state, setState ] = useState( () => {
+        const user = firebase.auth().currentUser;
+        
+        return { user, initializing: !user };
+    } );
+    
+    function onChange( user ){
+        setState( { initializing: false, user } );
+        
+        if( !user && history.location.pathname !== "/signup" &&
+            history.location.pathname !== "/login" &&
+            history.location.pathname !== "/root" ){
+            history.push( "/login" );
+        }
+    }
+    
+    React.useEffect( () => {
+        // listen for auth state changes
+        const unsubscribe = firebase.auth().onAuthStateChanged( onChange );
+        // unsubscribe to the listener when unmounting
+        return () => unsubscribe();
+    }, [] );
+    
+    return state;
+};
