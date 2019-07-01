dc5965e69835c7fde1ba2cc97e4fc09521dbe17f
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-07-01 16:15:43 -0700

    Makes header image background

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,13 +1,19 @@
-import React from "react";
-import headerImage from "../../assets/images/user-header-img.png";
+import React from "react"
+import headerImage from "../../assets/images/user-header-img.png"
 
 const Header = () => {
-    return ( <div className={ "dashboard_header" }>
-        <img className={ "dashboard_header-img" } src={ headerImage }/>
-        <h1 className={ "dashboard_header-user-name" }>Name Placeholder</h1>
-        <h3 className={ "dashboard_header-city" }>City Name</h3>
-        <i className="fas fa-trophy"></i>
-    </div> );
-};
+  return (
+    <div
+      className={"dashboard_header"}
+      style={{
+        background: `linear-gradient(180deg, rgba(0, 0, 0, 0.8), rgba(34, 34, 31, 1)), url('${headerImage}')`,
+      }}>
+      {/* <img className={ "dashboard_header-img" } src={ headerImage }/> */}
+      <h1 className={"dashboard_header-user-name"}>Name Placeholder</h1>
+      <h3 className={"dashboard_header-city"}>City Name</h3>
+      <i className="fas fa-trophy" />
+    </div>
+  )
+}
 
-export default Header;
+export default Header
