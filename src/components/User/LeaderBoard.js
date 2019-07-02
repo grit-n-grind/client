c05744fe2c5044a1986144c8bc2a37b7ab461919
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-07-02 06:39:26 -0700

    Create Gym styles

--- src/components/User/LeaderBoard.js
+++ src/components/User/LeaderBoard.js
@@ -2,21 +2,28 @@ import React from "react"
 import LeaderBoardUser from "./LeaderBoardUser"
 
 const users = [
-  { name: "Jeff" }, { name: "Martha" }, { name: "Bob" }, { name: "Jeff" },
-  { name: "Martha" }, { name: "Bob" }, { name: "Jeff" }, { name: "Martha" },
+  { name: "Jeff" },
+  { name: "Martha" },
+  { name: "Bob" },
+  { name: "Jeff" },
+  { name: "Martha" },
+  { name: "Bob" },
+  { name: "Jeff" },
+  { name: "Martha" },
   { name: "Bob" },
 ]
 
 const LeaderBoard = () => {
-  return ( <div className={ "leader_board" }>
-    <section className={ "leader_board_users" }>
-      { users && users.map( user => {
-        return <LeaderBoardUser name={ user.name }/>
-      } ) }
-    </section>
-  
-  
-  </div> )
+  return (
+    <div className={"leader_board"}>
+      <section className={"leader_board_users"}>
+        {users &&
+          users.map(user => {
+            return <LeaderBoardUser name={user.name} />
+          })}
+      </section>
+    </div>
+  )
 }
 
 export default LeaderBoard
