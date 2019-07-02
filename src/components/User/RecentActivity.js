1186e167736e7381c6fe6d25f91f971bab89322e
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-07-01 19:53:18 -0700

    onboarding styles

---
+++ src/components/User/RecentActivity.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+const RecentActivity = () => {
+  return (
+    <div>
+      <p>Recent activity</p>
+      <div>
+        {activities.map(activity => {
+          return (
+            // 
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default RecentActivity
