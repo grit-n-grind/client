8267261f2157fd38929729015671ec91abf2aab3
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-07-01 21:38:30 -0700

    Create GymForm component

--- src/App.js
+++ src/App.js
@@ -14,28 +14,33 @@ import UserDashboard from "./views/UserDashboard"
 
 import OwnerDashboard from "./views/OwnerDashboard"
 
-export default function App(){
+export default function App() {
   const reducer = useAuth()
-  console.log( reducer.state )
-  if( reducer.state.initializing ){
-    return ( <div>
+  console.log(reducer.state)
+  if (reducer.state.initializing) {
+    return (
+      <div>
         <h3>Loading...</h3>
-      </div> )
+      </div>
+    )
   }
-  if( reducer.state.onBoardUser ){
-    return ( <userContext.Provider value={ reducer }>
-        <OnBoarding/>
-      </userContext.Provider> )
+  if (reducer.state.onBoardUser) {
+    return (
+      <userContext.Provider value={reducer}>
+        <OnBoarding />
+      </userContext.Provider>
+    )
   }
-  return ( <userContext.Provider value={ reducer }>
+  return (
+    <userContext.Provider value={reducer}>
       <Switch>
         <PrivateRoute exact path="/dashboard" component={UserDashboard} />
+        <Route exact path="/dashboard/gym" component={OwnerDashboard} />
         <PrivateRoute exact path="/journals/new" component={AddJournal} />
         <Route exact path="/login" render={props => <AuthPage {...props} />} />
         <Route exact path="/signup" render={props => <AuthPage {...props} />} />
         <Route exact path="/" render={props => <Landing {...props} />} />
-
-        <Route exact path="/own-dash" component={OwnerDashboard} />
       </Switch>
-    </userContext.Provider> )
+    </userContext.Provider>
+  )
 }
