5692e17eecc6932213cc62e1684bd4a6b6bb301a
Maksim V <maks112v@gmail.com>
2019-07-02 00:32:52 -0400

    added landing buton

--- src/components/layout/Avatar.js
+++ src/components/layout/Avatar.js
@@ -2,9 +2,14 @@ import React from "react"
 
 const Avatar = ({ className, image }) => {
   return (
-    <div className={className}>
-      <img className={"avatar_image"} src={image} />
-    </div>
+    <div
+      className={className}
+      style={{
+        backgroundImage: `url(${image})`,
+        backgroundPosition: "center center",
+        backgroundSize: "cover",
+      }}
+    />
   )
 }
 
