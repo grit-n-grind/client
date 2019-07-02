81a9579733f796cded9c4b71aeebc88d1ca10351
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-07-01 22:54:43 -0700

    Recent activity

--- src/views/UserDashboard.js
+++ src/views/UserDashboard.js
@@ -2,22 +2,29 @@ import React, { useState } from "react"
 import Sidebar from "../components/layout/Sidebar"
 import Header from "../components/layout/Header"
 import Avatar from "../components/layout/Avatar"
+import RecentActivity from "../components/User/RecentActivity"
 import DashboardContent from "../components/User/DashboardContent"
 import { useSession } from "../hooks/useAuth"
 
 const UserDashboard = () => {
-  const [ page, setPage ] = useState( "goals" )
-  const { user, auth: { photoURL } } = useSession()
-  
-  return ( <div className={ "user_dashboard" }>
-    <Sidebar>
-      <Avatar className={ "avatar" } image={ photoURL }/>
-    </Sidebar>
-    <section className={ "user_dashboard-content" }>
-      <Header { ...user } page={ page } setPage={ setPage }/>
-      <DashboardContent page={ page }/>
-    </section>
-  </div> )
+  const [page, setPage] = useState("goals")
+  const {
+    user,
+    auth: { photoURL },
+  } = useSession()
+
+  return (
+    <div className={"user_dashboard"}>
+      <Sidebar>
+        <Avatar className={"avatar"} image={photoURL} />
+        <RecentActivity />
+      </Sidebar>
+      <section className={"user_dashboard-content"}>
+        <Header {...user} page={page} setPage={setPage} />
+        <DashboardContent page={page} />
+      </section>
+    </div>
+  )
 }
 
 export default UserDashboard
