3979809d65ed8f7985bc39fbb9ac8dc80b7693bb
Maksim V <maks112v@gmail.com>
2019-07-03 00:27:26 -0400

    added gym cards

--- src/components/Gym/CurrentCompetitions.js
+++ src/components/Gym/CurrentCompetitions.js
@@ -3,18 +3,32 @@ import CompetitionCard from "../Competition/CompetitionCard"
 import { useCompetitions } from "../../hooks/useCompetitions"
 import { useSession } from "../../hooks/useAuth"
 
-const CurrentCompetitions = ( { gym } ) => {
-  
-  const [ isLoading, competitions, addCompetitions, deleteComp ] = useCompetitions(
-    gym )
-  
-  return ( <div className={ "current_competitions" }>
+const CurrentCompetitions = ({ gym }) => {
+  const [
+    isLoading,
+    competitions,
+    addCompetitions,
+    deleteComp,
+  ] = useCompetitions(gym)
 
-    { competitions ? Object.values( competitions ).map( competition => {
-      return <CompetitionCard key={ competition.name }
-                              competition={ competition }/>
-    } ) : <><CompetitionCard /><CompetitionCard /><CompetitionCard /><CompetitionCard /></>}
-  </div> )
+  return (
+    <div className={"current_competitions"}>
+      {competitions ? (
+        Object.values(competitions).map(competition => {
+          return (
+            <CompetitionCard key={competition.name} competition={competition} />
+          )
+        })
+      ) : (
+        <>
+          <CompetitionCard />
+          <CompetitionCard />
+          <CompetitionCard />
+          <CompetitionCard />
+        </>
+      )}
+    </div>
+  )
 }
 
 export default CurrentCompetitions
---
+++ src/components/User/AllGyms.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react"
+import GymCard from "../Gym/GymCard"
+import { useSession } from "../../hooks/useAuth"
+
+import { store } from "../../config/firebase"
+
+const AllGyms = ({ gym }) => {
+  const [state, setstate] = useState({ isLoading: true, gyms: [] })
+
+  useEffect(() => {
+    const unsubscribe = store.collection("gyms").onSnapshot(snapshot => {
+      let items = []
+      snapshot.forEach(doc => {
+        const data = doc.data()
+        items.push({ location: `${data.city}, ${data.state}`, ...data })
+      })
+      setstate({ ...state, isLoading: false, gyms: items })
+    })
+    return () => unsubscribe()
+  }, [])
+
+  console.log(state)
+  return (
+    <div className={"current_competitions"}>
+      {state.gyms.map(gym => (
+        <GymCard gym={gym} />
+      ))}
+    </div>
+  )
+}
+
+export default AllGyms
