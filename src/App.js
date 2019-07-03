c919d592d0453ff19e5c17435782cc6c4d69bb48
Maksim V <maks112v@gmail.com>
2019-07-02 23:57:47 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Route, Switch } from "react-router-dom"
+import { Route, Switch, Redirect } from "react-router-dom"
 import { userContext, useAuth } from "./hooks/useAuth"
 
 // Componentss
@@ -50,6 +50,7 @@ export default function App() {
           path="/gym/:slug"
           render={props => <SingleGym {...props} />}
         />
+        <Redirect to="/dashboard" />
       </Switch>
     </userContext.Provider>
   )
